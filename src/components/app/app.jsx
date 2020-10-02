f1ed9b50babaedd31264b3c6ab818063d5d3e9f5
alex <dor_alex@inbox.ru>
2020-10-03 03:07:42 +0700

    module2-task1

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import Main from "../main/main";
+import Favorites from "../favorites/favorites";
+import Room from "../room/room";
+import SignIn from "../sign-in/sign-in";
 
 const App = (props) => {
   const {placesCount} = props;
 
   return (
-    <Main placesCount={placesCount}/>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Main placesCount={placesCount} />
+        </Route>
+        <Route exact path="/favorites">
+          <Favorites />
+        </Route>
+        <Route exact path="/login">
+          <SignIn />
+        </Route>
+        <Route exact path="/offer/:id">
+          <Room />
+        </Route>
+        <Route>
+          <h2>Page 404</h2>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
